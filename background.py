3649b7c46d7ebe62932a69b003c0e4c8cc246a55
dstrickla <DanielStricklandjr13@gmail.com>
2024-10-20 19:38:23 -0700

    Refactor background.py blit_background

--- background.py
+++ background.py
@@ -2,8 +2,15 @@ from pygame import sprite
 from pygame import image
 
 class Background(sprite.Sprite):
-    def __init__(self, image_file, location):
+    def __init__(self, game, image_file, location):
         sprite.Sprite.__init__(self)
+
+        self.screen = game.screen 
+        self.screen_rect = game.screen.get_rect()
+
         self.image = image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location 
+
+    def blit_background(self):
+        self.screen.blit(self.image, self.rect)
--- run_game.py
+++ run_game.py
@@ -18,8 +18,9 @@ class CandleInvader:
             self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Candle Invader")
 
-        self.background = Background(self.settings.background_path,
-                                      self.settings.background_cord)
+        self.background = Background(self, 
+                                     self.settings.background_path,
+                                     self.settings.background_cord)
         self.player = Player(self, self.settings.player_path)
 
     def run_game(self):
@@ -29,9 +30,8 @@ class CandleInvader:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-            self.screen.fill([255, 255, 255])
-            self.screen.blit(self.background.image, self.background.rect)
+            
+            self.background.blit_background()
             self.player.blit_player()
 
             pygame.display.flip()
